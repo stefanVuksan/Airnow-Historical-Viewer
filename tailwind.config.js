9ee06d5541ac94005667ce75db27e04d133efc8a
Stefan <stefa.vuksan.9881@gmail.com>
2023-01-23 07:27:26 -0800

    UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,12 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
