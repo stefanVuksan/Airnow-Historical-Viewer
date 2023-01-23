cb7f501f41881ff558426d7da4bb7c4cdba7cbf2
Stefan <stefa.vuksan.9881@gmail.com>
2023-01-23 11:53:34 -0800

    fix netlify issues

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
-export default {
+const config = {
   airnowapiURL: 'https://www.airnowapi.org/aq/observation/zipCode/historical/?format=application/json',
   APIKey: '17D4CC19-1913-42FA-9FA6-19289EDD329D',
 }
+
+export default config;
